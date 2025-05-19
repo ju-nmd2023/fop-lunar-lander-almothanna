f5f37597b3d324581a52dbbdf6d69140d83313eb
almothanna <148761376+almothanna@users.noreply.github.com>
2025-05-20 00:45:10 +0200

    Added thrust and result page, but the ship just crashes anyways, trying to find a solution and working on it.

--- lander.js
+++ lander.js
@@ -1,27 +1,34 @@
 class Lander {
-    constructor() 
-    {
+    constructor() {
+
       this.x = width / 2;
       this.y = 100;
       this.vel = 0;
       this.size = 40;
+
     }
   
-    update()
-    
-    {
+    update() {
+        
       this.vel += gravity;
       this.y += this.vel;
     }
   
-    display() 
-    
-    {
+    display() {
 
       fill(255);
       ellipse(this.x, this.y, this.size);
       textSize(14);
-      text("speed: " + this.vel.toFixed(2), 10, 20);
+      text("Speed: " + this.vel.toFixed(2), 10, 20 );
+
+    }
+  
+    applyThrust() {
+        
+      this.vel -= 0.2;
+      fill(255, 150, 0);
+      triangle(this.x - 10, this.y + this.size / 2, this.x + 10, this.y + this.size / 2, this.x, this.y + this.size / 2 + 20);
+
     }
   }
   
--- sketch.js
+++ sketch.js
@@ -5,7 +5,6 @@ let lander;
 let gravity = 0.1;
 
 function setup() {
-
   createCanvas(600, 600);
   lander = new Lander();
 }
@@ -17,33 +16,55 @@ function draw() {
     fill(255);
     textAlign(CENTER, CENTER);
     textSize(32);
-    text("Welcome to lunar lander game!! :)", width / 2, height / 2 - 40);
+    text("Welcome to Lunar Lander! :)", width / 2, height / 2 - 40);
     textSize(18);
-    text("Click any key to play!", width / 2, height / 2);
-
+    text("Click any key to start playing!", width / 2, height / 2);
   } else if (state === "play") {
     drawMoon();
     lander.update();
     lander.display();
-
+    checkLanding();
+  } else if (state === "result") {
+    fill(255);
+    textAlign(CENTER, CENTER);
+    textSize(28);
+    text(resultText, width / 2, height / 2);
+    textSize(18);
+    text("Click any key to play again!", width / 2, height / 2 + 40);
   }
 }
 
 function drawMoon() {
-
   fill(80);
   rect(0, groundY, width, height - groundY);
   fill(60);
   ellipse(100, groundY + 20, 40, 15);
   ellipse(300, groundY + 10, 60, 20);
   ellipse(500, groundY + 25, 35, 12);
-
-
 }
+
+let resultText = "";
 
 function keyPressed() {
-
-  if (state === "start") {
+  if (state === "start" || state === "result") {
+    lander = new Lander();
     state = "play";
   }
+  if (state === "play") {
+    lander.applyThrust();
+  }
 }
+
+function checkLanding() {
+  if (lander.y + lander.size / 2 >= groundY) {
+    if (lander.vel < 2) {
+      resultText = "You landed safely! Yay!!";
+    } else {
+      resultText = "You crashed! :'(";
+    }
+    state = "result";
+  }
+}
+
+
+
